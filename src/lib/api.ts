7b4acb4515633c8763e5519c2d4dfe6f6bbcaab8
seungchan <superfly9@encar.com>
2025-03-11 12:15:04 +0900

    feat: 카테고리 별 post 구분 추가

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,13 +5,26 @@ import { join } from "path";
 
 const postsDirectory = join(process.cwd(), "_posts");
 
-export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory);
+export function getPostWithCategory() {
+  const categories =  fs.readdirSync(postsDirectory).filter(category=>
+    fs.statSync(join(postsDirectory, category)).isDirectory());
+
+  let slugs = []
+  for (const category of categories) {
+    const categoryPath = join(postsDirectory, category);
+    const files = fs.readdirSync(categoryPath).filter(file=>file.endsWith('.md'));
+    slugs.push(...files.map((file)=>`${category}/${file}`))
+  }
+  return slugs;
 }
 
 export function getPostBySlug(slug: string) {
-  const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = join(postsDirectory, `${realSlug}.md`);
+  const [category, realSlug] = slug.replace(/\.md$/, "").split("/")
+  const fullPath = join(postsDirectory,category ,`${realSlug}.md`);
+  if (!fs.existsSync(fullPath)) {
+    return null;
+  }
+
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
@@ -19,10 +32,10 @@ export function getPostBySlug(slug: string) {
 }
 
 export function getAllPosts(): Post[] {
-  const slugs = getPostSlugs();
+  const slugs = getPostWithCategory(); // [ 'dev/dynamic-routing.md', 'dev/hello-world.md', 'dev/preview.md' ]
   const posts = slugs
     .map((slug) => getPostBySlug(slug))
-    // sort posts by date in descending order
+    .filter(((post): post is Post=>post !==null))
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
 }
