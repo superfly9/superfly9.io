432f5da8b73fb5d7864ed850aed8eae9ec51a22e
송승찬 <superfly09@daum.net>
2025-04-02 23:41:23 +0900

    feat: url querystring 필터에 추가

--- src/components/SearchFilter.tsx
+++ src/components/SearchFilter.tsx
@@ -4,35 +4,56 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { MoreStories } from "@/app/_components/more-stories";
 import { Input } from "@/components/ui/input";
 import { Post } from "@/interfaces/post";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
 
 interface Props {
   allPosts: Post[];
 }
 
 export default function SearchFilter({ allPosts }: Props) {
-  const [term, setTerm] = useState("");
   const [filtered, setFiltered] = useState<Post[]>(allPosts);
+
+  const searchParams = useSearchParams();
+  const [searchTerm, setSearchTerm] = useState(
+    searchParams.get("search") || ""
+  );
+  const router = useRouter();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setTerm(value);
+    setSearchTerm(value);
 
-    const lower = value.toLowerCase();
-    const filteredPosts = allPosts.filter(
-      (post) =>
-        post.title.toLowerCase().includes(lower) ||
-        post.content.toLowerCase().includes(lower)
-    );
-    setFiltered(filteredPosts);
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+
+    router.replace(`?${params.toString()}`);
   };
+
+  useEffect(() => {
+    if (searchTerm) {
+      setSearchTerm(searchTerm);
+      const lower = searchTerm.toLowerCase();
+      const filteredPosts = allPosts.filter(
+        (post) =>
+          post.title.toLowerCase().includes(lower) ||
+          post.content.toLowerCase().includes(lower)
+      );
+      setFiltered(filteredPosts);
+    }
+  }, [searchTerm]);
 
   return (
     <>
       <div className="mb-8">
         <Input
-          placeholder="검색어를 입력하세요..."
-          value={term}
+          placeholder="검색어를 입력하세요."
+          value={searchTerm}
           onChange={handleChange}
           className="w-full"
         />
